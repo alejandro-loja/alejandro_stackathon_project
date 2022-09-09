9d663d5e1fe5d5242da367d5e12a3cbdddfe1c4b
Alejandro Loja <alejandro.loja@gmail.com>
2022-09-09 00:32:34 -0400

    color change in nav bar for role

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -3,50 +3,63 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { logout } from "../store";
 
-const Navbar = ({ handleClick, isLoggedIn, auth }) => (
-  <nav className="navbar navbar-expand-lg bg-secondary mb-4 p-4 ">
-    <div className="container-fluid">
-      {isLoggedIn ? (
-        <div>
-          <span className="navbar-brand text-white fw-bold">
-            Hi, {auth.username}
-          </span>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home" className="link-light">
-            Home
-          </Link>
-          {auth.role === "Technician" && (
-            <Link className="link-light" to="/tasks">
-              Assigned Tasks
+const Navbar = ({ handleClick, isLoggedIn, auth }) => {
+  const color = () => {
+    if (auth.role === "Technician") {
+      return "bg-success";
+    } else if (auth.role === "Manager") {
+      return "bg-primary";
+    } else if (auth.role === "Supervisor") {
+      return "bg-dark";
+    } else {
+      return "bg-secondary";
+    }
+  };
+  return (
+    <nav className={`navbar navbar-expand-lg ${color()} mb-4 p-4 `}>
+      <div className="container-fluid">
+        {isLoggedIn ? (
+          <div>
+            <span className="navbar-brand text-white fw-bold">
+              Hi, {auth.username}
+            </span>
+            {/* The navbar will show these links after you log in */}
+            <Link to="/home" className="link-light">
+              Home
             </Link>
-          )}
-          {auth.role !== "Technician" && (
-            <Link className="link-light" to="/tasks">
-              Tasks
+            {auth.role === "Technician" && (
+              <Link className="link-light" to="/tasks">
+                Assigned Tasks
+              </Link>
+            )}
+            {auth.role !== "Technician" && (
+              <Link className="link-light" to="/tasks">
+                Tasks
+              </Link>
+            )}
+            {auth.role === "Supervisor" && (
+              <Link className="link-light" to="/users">
+                Employees
+              </Link>
+            )}
+            <a href="#" className="link-light" onClick={handleClick}>
+              Logout
+            </a>
+            <span className="navbar-text text-end">Role: {auth.role}</span>
+          </div>
+        ) : (
+          <div>
+            {/* The navbar will show these links before you log in */}
+            <Link className="link-light" to="/login">
+              Login
             </Link>
-          )}
-          {auth.role === "Supervisor" && (
-            <Link className="link-light" to="/users">
-              Employees
-            </Link>
-          )}
-          <a href="#" className="link-light" onClick={handleClick}>
-            Logout
-          </a>
-          <span className="navbar-text text-end">Role: {auth.role}</span>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link className="link-light" to="/login">
-            Login
-          </Link>
-          {/* <Link to="/signup">Sign Up</Link> */}
-        </div>
-      )}
-    </div>
-  </nav>
-);
+            {/* <Link to="/signup">Sign Up</Link> */}
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
 
 /**
  * CONTAINER
