f62005afe1346d127c21bfa213dea5b4c3c01537
Alejandro Loja <alejandro.loja@gmail.com>
2023-05-23 22:12:11 -0400

    remove styling

--- client/Routes.js
+++ client/Routes.js
@@ -24,7 +24,7 @@ class Routes extends Component {
 
     return (
       <>
-        <h1 className="text-red-500">Hello test</h1>
+        <h1>Hello</h1>
         {isLoggedIn ? (
           <Switch>
             <Route path="/home" component={Home} />
